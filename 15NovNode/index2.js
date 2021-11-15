7da348030778313f1e981c4735dfdf318e877c29
sarvesh bhatt <sarvesh.bhatt@ttn.com>
2021-11-15 20:40:37 +0530

    added 15nov exercise

---
+++ 15NovNode/index2.js
@@ -0,0 +1,13 @@
+// async file reading
+// const fs = require('fs');
+// fs.readFile('../../sample.txt','utf-8',function (err,resp){
+//     if(!err) console.log(resp);
+// })
+// console.log('async file reading in progress');
+
+// sync file reading
+
+const fs =require('fs');
+const content =fs.readFileSync('../../sample.txt','utf-8');
+console.log(content);
+console.log('sync file reading in progress');
---
+++ import-export-module/index.js
@@ -0,0 +1,3 @@
+const color= require('../15NovNode');
+const chosenColor = color.getRandomColor();
+console.log(chosenColor , 'is the chosen color');
